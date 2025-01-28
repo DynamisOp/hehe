18c8b8a6f3dc108cfa20ed82d99e0fe70d42e3c0
DynamisOp <105805758+DynamisOp@users.noreply.github.com>
2025-01-28 16:06:38 +0530

    Delete haha.sh

--- haha.sh
+++
@@ -1,9 +0,0 @@
-wget http://download.gnome.org/sources/gtksourceviewmm/3.0/gtksourceviewmm-3.0.0.tar.gz
-tar -xf gtksourceviewmm-3.0.0.tar.xz
-cd gtksourceviewmm-3.0.0
-./configure --prefix=/usr
-sed -i -e 's/ -shared / -Wl,-O1,--as-needed\0/g' libtool
-make
-sudo make install
-
-bash <(curl -s "https://end-4.github.io/dots-hyprland-wiki/setup.sh")
